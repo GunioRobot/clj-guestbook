23dc93d979371bd72d8867805fa6e96ff52f54e5
jneira <atreyu.bbb@gmail.com>
2010-03-06 22:00:02 +0100

    'snd version'

--- war/WEB-INF/classes/clj/guestbook/servlet.clj
+++ war/WEB-INF/classes/clj/guestbook/servlet.clj
@@ -1,10 +1,26 @@
 (ns clj.guestbook.servlet
   (:gen-class :extends javax.servlet.http.HttpServlet)
   (:use [compojure.http servlet routes])
-  (:use compojure.html))
+  (:use compojure.html)
+  (:import
+    (com.google.appengine.api.users UserServiceFactory)))
 
 (defroutes clj-guestbook
   (GET "/clj/hello"
-    (html [:h1 "Hello, World!"])))
+    (html [:h1 "Hola, petardo!"]))
+
+  (GET "/clj/user"
+    (let [user-service (UserServiceFactory/getUserService)
+          user (.getCurrentUser user-service)]
+      (html [:h1 "Hello, " (if user (.getNickname user) "World") "!"])))
+
+  (GET "/clj/user/hello"
+     ( let [user-service (UserServiceFactory/getUserService)
+            user (.getCurrentUser user-service)]
+      (html
+        [:h1 "Hello, " (if user (.getNickname user) "World") "!"]
+        [:p (link-to (.createLoginURL user-service "/clj/user") "sign in")]
+        [:p (link-to (.createLogoutURL user-service "/clj/user") "sign out")]))))
+
 
 (defservice clj-guestbook)
